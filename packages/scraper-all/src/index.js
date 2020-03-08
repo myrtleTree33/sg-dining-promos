20b2ddeb1197526b1b54bd65c30597cf0a864233
jhtong <me@joeltong.org>
2020-03-08 23:40:40 +0800

    added scraper-all

---
+++ packages/scraper-all/src/index.js
@@ -0,0 +1,9 @@
+import { logger } from './utils/logger';
+
+const {} = process.env;
+
+// Add your code here
+
+// TODO use a job queue like here: https://github.com/OptimalBits/bull
+
+logger.info('Hello world!');
